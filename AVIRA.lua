601e6523e2c513a02d3e28fcc189e130ad408650
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-08-25 05:20:30 +0200

    Add files via upload

--- AVIRA.lua
+++ AVIRA.lua
@@ -2739,7 +2739,7 @@ end
 if text == 'المطور' or text == 'مطور' then 
 tdcli_function ({ID = "GetUser",user_id_ = SUDO},function(arg,result)  
 local msg_id = msg.id_/2097152/0.5 
-local Text = "DEV NAME -> ["..result.first_name_.."](tg://user?id="..SUDO..")\nDEV ID -> "..SUDO.."\nDEV USER -> @"..result.username_
+local Text = "DEV NAME -> ["..result.first_name_.."](tg://user?id="..SUDO..")\nDEV USER -> [@"..result.username_.."]\nDEV ID -> `"..SUDO.."`"
 keyboard = {}  
 keyboard.inline_keyboard = { 
 {{text = '❲'..result.first_name_..'❳',url="t.me/"..result.username_}}, 
