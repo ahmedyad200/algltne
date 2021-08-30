55a51c72c04e656cb385c6471eee564455b83fd1
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-08-30 16:45:49 +0200

    Add files via upload

--- File_Bot/rdodsudos.lua
+++ File_Bot/rdodsudos.lua
@@ -2,7 +2,6 @@ local function rdodsudos(msg)
 local text = msg.content_.text_
 
 
-
 if text == 'مين نصبلك' or text == 'عايزه بوت' or text == 'عايز بوت' then
 local Text = [[  
 ◍ لو عايز بوت مميز بدون توقف وامان  .
@@ -35,7 +34,6 @@ https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. ms
 end
 
 
-
 if text == 'بويكا الكبير' or text == 'بويكا اليكس' or text == 'بويكا الكبير' or text == 'بويكا اليكس' or text == 'صاحب الابليكيشن' or text == 'مبرمج السورس'  then
 local Text = [[
 المبرمج بويكا لو حابب تتواصل معاه
@@ -49,6 +47,8 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/Boyka_alex&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
+
+
 if text == 'ارهابي' or text == 'ارهابي الكبير' or text == 'صاحب السورس' or text == 'ارهابي القلوب' or text == 'ارهابي قلوب' or text == 'صاحب السورس' or text =='الارهابي' then
 local Text = [[ 
 مطور السورس يمكنك التواصل معه
@@ -60,6 +60,8 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/F_R_M1/510&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
+
+
 if text == 'صاصا' or text == 'صاصا الكبير'  then
 local Text = [[ 
 مطور السورس يمكنك التواصل معه
@@ -71,6 +73,8 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/S_A_S_A_1&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
+
+
 if text == 'الكومندا' or text == 'الكومندا التالت'  then
 local Text = [[ 
 مطور السورس يمكنك التواصل معه
@@ -82,6 +86,8 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/komanda3b&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
+
+
 if text == 'تاكي' or text == 'تاكي الكبير' or text == 'تاكي السويس' or text == 'تاكي ص السويس' or text == 'تاكي الكبير' or text == 'تاكي' or text =='تاكي بتع السويس' then
 local Text = [[
 مطور السورس للتواصل اضغط علي الزر
